e04d9778e32888e89d6b1e49dc1acf28831f44cb
ihsan <ihsan.android@gmail.com>
2025-05-13 13:43:44 +0700

    feat(tutorial): chapter 1 initiate web server

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// logic web
+		fmt.Fprint(w, "hello world")
+	})
+
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "hi")
+	})
+
+	mux.HandleFunc("/request", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.Method)
+		fmt.Fprint(w, r.RequestURI)
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
