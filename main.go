6903bac1dab44ea7d46ddca650cc3fe75b078648
ihsan <ihsan.android@gmail.com>
2025-05-19 14:42:40 +0700

    feat(tutorial): file server and embed files

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"net/http"
 )
 
+//go:embed resources
+var resources embed.FS
+
 func main() {
+	// directory := http.Dir("./resources")
+	directory, _ := fs.Sub(resources, "resources")
+	fileServer := http.FileServer(http.FS(directory))
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", HelloHandler)
@@ -17,6 +25,9 @@ func main() {
 	mux.HandleFunc("/response-code", ResponseCodeHandler)
 	mux.HandleFunc("/set-cookie", SetCookieHandler)
 	mux.HandleFunc("/get-cookie", GetCookieHandler)
+
+	// static file from resources folder
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	server := http.Server{
 		Addr:    "localhost:8080",
