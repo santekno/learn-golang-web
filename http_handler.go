3284bf35e27719a8662a7baf041322162b5431d5
ihsan <ihsan.android@gmail.com>
2025-06-29 13:44:23 +0700

    add for template

--- http_handler.go
+++ http_handler.go
@@ -178,3 +178,15 @@ func TemplateActionWithHandler(w http.ResponseWriter, r *http.Request) {
 		},
 	})
 }
+
+func TemplateLayoutHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"./templates/header.html",
+		"./templates/footer.html",
+		"./templates/layout.html",
+	))
+	t.ExecuteTemplate(w, "layout", map[string]interface{}{
+		"Name":  "Santekno",
+		"Title": "Contoh Template Layout",
+	})
+}
--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 	mux.HandleFunc("/template-action-comparator", TemplateActionComparatorHandler)
 	mux.HandleFunc("/template-action-range", TemplateActionRangeHandler)
 	mux.HandleFunc("/template-action-with", TemplateActionWithHandler)
+	mux.HandleFunc("/template-layout", TemplateLayoutHandler)
 
 	server := http.Server{
 		Addr:    "localhost:8080",
